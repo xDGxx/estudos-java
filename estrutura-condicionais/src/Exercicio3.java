7586eb2f7dddb9bc9d84abe11de4c962368a26b0
xDGxx <cntt.dg@gmail.com>
2025-04-03 12:31:40 -0300

    Exercicio 3 concluído

--- estrutura-condicionais/src/Exercicio3.java
+++ estrutura-condicionais/src/Exercicio3.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Exercicio3 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+
+        System.out.println("Digite o egundo valor: ");
+        b = scan.nextInt();
+
+        if (a % b == 0 && b % a == 0){
+            System.out.println("São múltiplos");
+        }else {
+            System.out.println("Não são múltiplos");
+        }
+    }
 }
