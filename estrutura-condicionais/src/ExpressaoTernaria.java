8ff34290999704d6ea169294008420672222f7c1
xDGxx <cntt.dg@gmail.com>
2025-04-08 15:26:44 -0300

    Utilizando operador ternario para desconto

--- estrutura-condicionais/src/ExpressaoTernaria.java
+++ estrutura-condicionais/src/ExpressaoTernaria.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ExpressaoTernaria {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // ? = sim
+        // : = não
+
+        /*DEMO
+        double preco = 34.5;
+        double desconto;
+        if (preco < 20.0){
+            desconto = preco * 0.1;
+        } else {
+            desconto = preco * 0.05
+        }
+        */
+
+        //TERNARIA
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+
+    }
+
 }
