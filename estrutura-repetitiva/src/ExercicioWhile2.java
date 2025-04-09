05ef3c1a09c208430a6cf292bd8e51234487fc9c
xDGxx <cntt.dg@gmail.com>
2025-04-09 13:06:14 -0300

    Exercicio 2 em while

--- estrutura-repetitiva/src/ExercicioWhile2.java
+++ estrutura-repetitiva/src/ExercicioWhile2.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+//Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+// cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+// menos uma de duas coordenadas for NULA
+
+import java.util.Scanner;
 
 public class ExercicioWhile2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+
+        while (x != 0 && y != 0){
+            if (x > 0 && y > 0){
+                System.out.println("primeiro");
+            } else if (x < 0 && y > 0){
+                System.out.println("segundo");
+            } else if (x < 0 && y < 0){
+                System.out.println("terceiro");
+            } else {
+                System.out.println("quarto");
+            }
+
+            x = scan.nextInt();
+            y = scan.nextInt();
+
+        }
+
+    }
+
 }
