2dc7f3d9c33549cf906cfb75e8ab82bf201522c7
xDGxx <cntt.dg@gmail.com>
2025-04-11 11:10:15 -0300

    Primeiro exercicio em for

---
+++ estrutura-repetitiva/src/ExercicioFor1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ExercicioFor1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("DIGITE UM VALOR E DESCUBRA OS NÚMEROS PARES ATÉ ELE");
+        int x = scan.nextInt();
+
+        for ( int i = 0; i<=x; i++ ){
+            if ( i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
