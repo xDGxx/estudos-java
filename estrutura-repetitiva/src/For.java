f5f69acfa3e90891b0375f7e64d498dfb98b1859
xDGxx <cntt.dg@gmail.com>
2025-04-11 10:46:55 -0300

    Estudo de estrutura de repetição for em Java

---
+++ estrutura-repetitiva/src/For.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class For {
+
+    //SINTAXE
+    //for ( inicio ; condição ; incremento ) {
+    // comando 1
+    // comando 2
+    // } /*
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+        int soma = 0;
+
+        for ( int i = 0; i < num; i++){
+            int x = scan.nextInt();
+            soma += x;
+        }
+
+        System.out.println(soma);
+
+    }
+
+}
