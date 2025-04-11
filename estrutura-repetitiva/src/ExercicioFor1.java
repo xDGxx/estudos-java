b72e6ecedf4f0062646eb07e8d85d52253f8422d
xDGxx <cntt.dg@gmail.com>
2025-04-11 11:12:16 -0300

    Primeiro exercicio em for

--- estrutura-repetitiva/src/ExercicioFor1.java
+++ estrutura-repetitiva/src/ExercicioFor1.java
@@ -1,3 +1,6 @@
+//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+//X, se for o caso.
+
 import java.util.Scanner;
 
 public class ExercicioFor1 {
