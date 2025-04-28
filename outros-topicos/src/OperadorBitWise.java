21b720df1cc0ca9af5e1a18e062b63e79d3c692a
xDGxx <cntt.dg@gmail.com>
2025-04-28 14:33:13 -0300

    Entendendo operadores Bit a Bit em Java

--- outros-topicos/src/OperadorBitWise.java
+++ outros-topicos/src/OperadorBitWise.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class OperadorBitWise {
+
+    //Verificador de Bit a Bit onde:
+    // & = operador "E"
+    // | = operador "OU"
+    // ^ = operador "OU EXCLUSIVO" (onde apenas quando uma for verdadeira, irá dar true)
+
+    public static void main(String[] args) {
+
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+
+    }
+
 }
