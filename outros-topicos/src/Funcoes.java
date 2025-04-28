d3ff7eb0b7cc30ae7230e5498c4542a958a445a0
xDGxx <cntt.dg@gmail.com>
2025-04-28 14:34:00 -0300

    Desvendando o maior número com funções em Java

--- outros-topicos/src/Funcoes.java
+++ outros-topicos/src/Funcoes.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+//Fazer um programa que leia três números inteiros e mostre na tela o maior deles
+
+import java.util.Scanner;
 
 public class Funcoes {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter three numbers: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        int higher = max(a,b,c);
+
+        showResult(higher);
+
+    }
+
+        public static int max(int x, int y, int z){
+
+            int aux;
+            if (x > y && x > z){
+                aux = x;
+            }else if(y>z){
+                aux = y;
+            }else {
+                aux = z;
+            } return aux;
+        }
+
+        public static void showResult(int value){
+            System.out.println("Higher = " + value);
+    }
+
 }
