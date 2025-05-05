e27ef85591ece2fc21bfebf9a894df2dcea3057f
xDGxx <cntt.dg@gmail.com>
2025-05-05 11:49:00 -0300

    Atribuindo ao triângulo o método Math.sqrt para cálculo

--- poo/src/entities/Triangulo.java
+++ poo/src/entities/Triangulo.java
@@ -1,9 +1,20 @@
-package entities;
+package entities; //pacote da classe
 
-public class Triangulo {
+public class Triangulo { //Nome da classe
 
-    public double a;
-    public double b;
-    public double c;
+    public double a; //Atributo da classe
+    public double b; //Atributo da classe
+    public double c; //Atributo da classe
+
+
+    //public indica que o atributo ou metodo pode ser acesso em outros arquivos
+    public double area(){
+        //double informa o tipo de dado que o metodo retorna
+        //area é o nome do metodo
+        
+        //corpo do metodo
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p-a) * (p-b) * (p-c));
+    }
 
 }
