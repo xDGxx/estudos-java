7faff3c8d78f1f17d521622f0360571053ab2bb9
xDGxx <cntt.dg@gmail.com>
2025-05-05 11:50:30 -0300

    Atualizando o programa final otimizando o código

--- poo/src/application/Program.java
+++ poo/src/application/Program.java
@@ -26,11 +26,9 @@ public class Program {
         y.b = scan.nextDouble();
         y.c = scan.nextDouble();
 
-        double p = (x.a + x.b + x.c) / 2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (y.a + y.b + y.c) / 2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
         
         System.out.printf("Área do triângulo X: %.4f%n", areaX);
         System.out.printf("Área do triângulo Y: %.4f%n", areaY);
