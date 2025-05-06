d343939a81186722684a1549c1b1e0ada91ca653
xDGxx <cntt.dg@gmail.com>
2025-05-06 19:23:13 -0300

    Informações e regras para calculo de decremento e incremento do salario do funcionario

--- salary-calculation/src/entities/Employee.java
+++ salary-calculation/src/entities/Employee.java
@@ -1,4 +1,23 @@
 package entities;
 
 public class Employee {
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double nextSalary() { //Afirma que o proximo salario será o salario bruto com adicção da taxa
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary (double percentage){ // Incremento do salario com a porcentagem
+        grossSalary += grossSalary * percentage / 100;
+    }
+
+    public String toString(){ //Impressão em toString dos dados do funcionario
+        return name
+                + ", $ "
+                + String.format("%.2f", nextSalary());
+    }
+
 }
