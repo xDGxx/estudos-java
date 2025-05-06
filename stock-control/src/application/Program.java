28069a9b7dee287049eee209fa5bd10a01ade0c3
xDGxx <cntt.dg@gmail.com>
2025-05-06 18:43:05 -0300

    Adicionando e imprimindo o conteúdo

--- stock-control/src/application/Program.java
+++ stock-control/src/application/Program.java
@@ -17,8 +17,29 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity= scan.nextInt();
 
-        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        //System.out.println(product); = ao tentar realizar essa
+        // operação não mostrará a string e sim o nome product + números
 
+        // Para que seja visivel como String, devemos criar a função
+        // toString em Product pois a mesma também é um objeto
 
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = scan.nextInt(); //Cria a variavel e solicita uma quantidade
+        product.addProducts(quantity); //Chama a variavel de adição e mostra a nova quantidade adicionada
+
+        System.out.println();
+        System.out.println("Update data: " + product); //Mostra product com dados atualizados
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = scan.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product);
     }
 }
