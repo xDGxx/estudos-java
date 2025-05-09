d016189b659713d8b591282de0e2e64c3d8b0d76
xDGxx <cntt.dg@gmail.com>
2025-05-08 22:14:55 -0300

    adição do pacote util com a classe Calculator com membros estaticos sendo declarados por funções

--- membros-estaticos/src/util/Calculator.java
+++ membros-estaticos/src/util/Calculator.java
@@ -1,4 +1,15 @@
 package util;
 
 public class Calculator {
+
+    public static final double PI = 3.14159; //O final determina que será uma constante, padrão de constantes é sempre maiúscula
+
+    public static double circumference ( double radius){
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume (double radius){
+        return 4.0 * PI * radius * radius * radius / 3;
+    }
+
 }
