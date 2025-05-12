f92fb5bffbc8d8a4b7dca5274d3eeb2f8bb04088
xDGxx <cntt.dg@gmail.com>
2025-05-11 23:43:02 -0300

    Regra de negócio para converter a moeda

--- currency-converter/src/util/CurrencyConverter.java
+++ currency-converter/src/util/CurrencyConverter.java
@@ -1,4 +1,23 @@
 package util;
 
 public class CurrencyConverter {
+
+    //Preço do dolar
+    public static double dollarPrice;
+
+    //Define a cotação do dolar
+    public static void setDollarPrice(double price){
+        dollarPrice = price;
+    }
+
+    //Define a quantidade de dolares compraveis
+    public static double calculateDollars( double amountInReais){
+        return amountInReais / (dollarPrice * 1.06);
+    }
+
+    //Calcula o valor a ser pago em reais
+    public static double calculateAmountInReais (double dollarsToBuy){
+        return dollarsToBuy * dollarPrice * 1.06;
+    }
+
 }
