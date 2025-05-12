df7bb0b05edca7c5a4a5ed2d0c36c156b98b286b
xDGxx <cntt.dg@gmail.com>
2025-05-12 13:42:52 -0300

    Adicionando a classe Product com construtores

--- construtores/src/application/Program.java
+++ construtores/src/application/Program.java
@@ -1,4 +1,38 @@
 package application;
 
+import entities.Product;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = scan.nextLine(); //Variavel temporaria
+        System.out.print("Price: ");
+        double price = scan.nextDouble(); //Variavel temporaria
+
+        Product product = new Product(name, price); //Criação do objeto product com os construtores definidos
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = scan.nextInt(); //Cria a variavel e solicita uma quantidade
+        product.addProducts(quantity); //Chama a variavel de adição e mostra a nova quantidade adicionada
+
+        System.out.println();
+        System.out.println("Update data: " + product); //Mostra product com dados atualizados
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = scan.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product);
+    }
 }
